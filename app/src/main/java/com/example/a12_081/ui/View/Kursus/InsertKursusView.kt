6107cb6369c5639e7c5e898c419ef93ea56ceb8d
Namira Pramadhani <namnam6617@gmail.com>
2025-01-26 01:04:04 +0700

    Membuat DestinasiInsertKursus di insert view

--- app/src/main/java/com/example/a12_081/ui/View/Kursus/InsertKursusView.kt
+++ app/src/main/java/com/example/a12_081/ui/View/Kursus/InsertKursusView.kt
@@ -1,2 +1,8 @@
 package com.example.a12_081.ui.View.Kursus
 
+import com.example.a12_081.ui.Navigation.AlamatNavigasi
+
+object DestinasiInsertKursus: AlamatNavigasi {
+    override val route= "InsertKursus"
+    override val titleRes = "Tambah Kursus"
+}
