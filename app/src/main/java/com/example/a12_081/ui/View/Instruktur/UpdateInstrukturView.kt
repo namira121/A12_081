1a48f471167b4b74058dc36a7f139142e7136fb9
Namira Pramadhani <namnam6617@gmail.com>
2025-01-26 00:56:58 +0700

    Membuat DestinasiUpdateInstruktur di update view

--- app/src/main/java/com/example/a12_081/ui/View/Instruktur/UpdateInstrukturView.kt
+++ app/src/main/java/com/example/a12_081/ui/View/Instruktur/UpdateInstrukturView.kt
@@ -1,2 +1,10 @@
 package com.example.a12_081.ui.View.Instruktur
 
+import com.example.a12_081.ui.Navigation.AlamatNavigasi
+
+object DestinasiUpdateInstruktur: AlamatNavigasi {
+    override val route= "UpdateInstruktur"
+    override val titleRes= "Update Instruktur"
+    const val ID_INSTRUKTUR = "id_instruktur"
+    val routeWithArgs = "$route/{$ID_INSTRUKTUR}"
+}
