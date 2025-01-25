e4d57dd5f8157bcb59df4552a36a0db4d72a6f4a
Namira Pramadhani <namnam6617@gmail.com>
2025-01-25 21:33:22 +0700

    membuat variabel untuk updatestate kursus dan mengecek apakah id null atau tidak

--- app/src/main/java/com/example/a12_081/ui/ViewModel/Kursus/UpdateKursusViewModel.kt
+++ app/src/main/java/com/example/a12_081/ui/ViewModel/Kursus/UpdateKursusViewModel.kt
@@ -1,10 +1,19 @@
 package com.example.a12_081.ui.ViewModel.Kursus
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.a12_081.model.kursus
 import com.example.a12_081.repository.KursusRepository
 
 class UpdateKursusViewModel (savedStateHandle: SavedStateHandle, private val kursusRepository: KursusRepository): ViewModel(){
+
+    var updateKursusUiState by mutableStateOf(InsertKursusUiState())
+        private set
+
+    private val _id_kursus: String = checkNotNull(savedStateHandle[DestinasiUpdateKursus.ID_KURSUS])
+
 
 }
