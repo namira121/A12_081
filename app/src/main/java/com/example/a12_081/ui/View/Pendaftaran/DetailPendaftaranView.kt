9336cc91c2f652b9c8959975dae44a5b4d23d883
Namira Pramadhani <namnam6617@gmail.com>
2025-01-26 01:08:54 +0700

    Menambahkan alamat navigasi untuk halaman DetailPendaftaranView.kt

--- app/src/main/java/com/example/a12_081/ui/View/Pendaftaran/DetailPendaftaranView.kt
+++ app/src/main/java/com/example/a12_081/ui/View/Pendaftaran/DetailPendaftaranView.kt
@@ -1,2 +1,10 @@
 package com.example.a12_081.ui.View.Pendaftaran
 
+import com.example.a12_081.ui.Navigation.AlamatNavigasi
+
+object DestinasiDetailPendaftaran: AlamatNavigasi {
+    override val route= "DetailPendaftaran"
+    override val titleRes= "Detail Data Pendaftaran"
+    const val ID_PENDAFTARAN = "id_pendaftaran"
+    val routeWithArgs = "$route/{$ID_PENDAFTARAN}"
+}
