6663421aadbcd1794a2bd0140210f03eae819979
Namira Pramadhani <namnam6617@gmail.com>
2025-01-25 22:16:52 +0700

    membuat class DetailKursusViewModel untuk menjalankan operasi getkursusbyId, updatenya dan delete

--- app/src/main/java/com/example/a12_081/ui/ViewModel/Kursus/DetailKursusViewModel.kt
+++ app/src/main/java/com/example/a12_081/ui/ViewModel/Kursus/DetailKursusViewModel.kt
@@ -1,8 +1,16 @@
 package com.example.a12_081.ui.ViewModel.Kursus
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.a12_081.model.kursus
+import com.example.a12_081.repository.KursusRepository
+import kotlinx.coroutines.launch
 
-data class DetailKursusViewModel(
+data class DetailKursusUiState(
     val detailKursusUiEvent: InsertKursusUiEvent = InsertKursusUiEvent(),
     val isLoading: Boolean = false,
     val isError: Boolean = false,
@@ -23,4 +31,52 @@ fun kursus.toDetailKursusUiEvent(): InsertKursusUiEvent{
         harga = harga,
         id_instruktur= id_instruktur
     )
+}
+
+class DetailKursusViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val kursusRepository: KursusRepository
+): ViewModel(){
+    private val id_kursus:String = checkNotNull(savedStateHandle[DestinasiDetailKursus.ID_KURSUS])
+    var detailKursusUiState: DetailKursusUiState by mutableStateOf(DetailKursusUiState())
+        private set
+
+    init {
+        getKursusByID()
+    }
+
+    private fun getKursusByID(){
+        viewModelScope.launch {
+            detailKursusUiState = DetailKursusUiState(isLoading = true)
+            try {
+                val result = kursusRepository.getKursusByID(id_kursus)
+                detailKursusUiState = DetailKursusUiState(
+                    detailKursusUiEvent = result.toDetailKursusUiEvent(),
+                    isLoading = true
+                )
+            }catch (e:Exception){
+                detailKursusUiState = DetailKursusUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = e.message ?: "Unknown"
+                )
+            }
+        }
+    }
+
+    fun deleteKrs(){
+        viewModelScope.launch {
+            detailKursusUiState = DetailKursusUiState(isLoading =  true)
+            try {
+                kursusRepository.deleteKursus(id_kursus)
+                detailKursusUiState = DetailKursusUiState(isLoading = false)
+            }catch (e:Exception){
+                detailKursusUiState = DetailKursusUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = e.message?: :Unknowd Error
+                )
+            }
+        }
+    }
 }
