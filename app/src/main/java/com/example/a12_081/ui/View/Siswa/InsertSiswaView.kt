4e92c06b3f155de5413522fd3feb85b1026e0d42
Namira Pramadhani <namnam6617@gmail.com>
2025-01-25 22:52:46 +0700

    Menambahkan DestinasiInsertSiswa di halaman InsertSiswaView.kt

--- app/src/main/java/com/example/a12_081/ui/View/Siswa/InsertSiswaView.kt
+++ app/src/main/java/com/example/a12_081/ui/View/Siswa/InsertSiswaView.kt
@@ -1,2 +1,8 @@
 package com.example.a12_081.ui.View.Siswa
 
+import com.example.a12_081.ui.Navigation.AlamatNavigasi
+
+object DestinasiInsertSiswa: AlamatNavigasi{
+    override val route= "InsertSiswa"
+    override val titleRes = "Tambah Siswa"
+}
