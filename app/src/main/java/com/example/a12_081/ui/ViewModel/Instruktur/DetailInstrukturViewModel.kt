9c8659b59134fcc04bee0ea6d7438ee820a0edc2
Namira Pramadhani <namnam6617@gmail.com>
2025-01-25 22:25:17 +0700

    membuat fungsi toDetailInstrukturUiEvent

--- app/src/main/java/com/example/a12_081/ui/ViewModel/Instruktur/DetailInstrukturViewModel.kt
+++ app/src/main/java/com/example/a12_081/ui/ViewModel/Instruktur/DetailInstrukturViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.a12_081.ui.ViewModel.Instruktur
 
+import com.example.a12_081.model.instruktur
+
 data class DetailInstrukturUiState(
     val detailInstrukturUiEvent: InsertInstrukturUiEvent = InsertInstrukturUiEvent(),
     val isLoading: Boolean = false,
@@ -11,4 +13,14 @@ data class DetailInstrukturUiState(
 
     val isUiEventNotEmpty: Boolean
         get() = detailInstrukturUiEvent != InsertInstrukturUiEvent()
+}
+
+fun instruktur.toDetailInstrukturUiEvent(): InsertInstrukturUiEvent{
+    return InsertInstrukturUiEvent(
+        id_instruktur = id_instruktur,
+        nama_instruktur=nama_instruktur,
+        email=email,
+        nomor_telepon=nomor_telepon,
+        deskripsi=deskripsi
+    )
 }
