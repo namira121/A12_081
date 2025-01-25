f0b0659629897b784c8d1726b79525bd3587ed2a
Namira Pramadhani <namnam6617@gmail.com>
2025-01-25 18:47:08 +0700

    membuat data class untuk data yang akan ditambahkan

--- app/src/main/java/com/example/a12_081/ui/ViewModel/Kursus/InsertKursusViewModel.kt
+++ app/src/main/java/com/example/a12_081/ui/ViewModel/Kursus/InsertKursusViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.a12_081.ui.ViewModel.Kursus
 
+data class InsertKursusUiEvent(
+    val id_kursus: String = "",
+    val nama_kursus: String = "",
+    val deskripsi: String ="",
+    val kategori: String="",
+    val harga: String="",
+    val id_instruktur: String=""
+)
