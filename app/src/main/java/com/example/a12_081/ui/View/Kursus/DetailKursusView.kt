2fb7ee01d8d3ed1db4e5fb318c4a6a31290f9350
Namira Pramadhani <namnam6617@gmail.com>
2025-01-26 01:10:22 +0700

    Menambahkan alamat navigasi untuk halaman DetailKursusView.kt

--- app/src/main/java/com/example/a12_081/ui/View/Kursus/DetailKursusView.kt
+++ app/src/main/java/com/example/a12_081/ui/View/Kursus/DetailKursusView.kt
@@ -1,2 +1,10 @@
 package com.example.a12_081.ui.View.Kursus
 
+import com.example.a12_081.ui.Navigation.AlamatNavigasi
+
+object DestinasiDetailKursus: AlamatNavigasi {
+    override val route= "DetailKursus"
+    override val titleRes= "Detail Data Kursus"
+    const val ID_KURSUS = "id_kursus"
+    val routeWithArgs = "$route/{$ID_KURSUS}"
+}
