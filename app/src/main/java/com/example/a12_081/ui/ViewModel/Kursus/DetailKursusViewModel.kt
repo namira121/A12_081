77f5728ae2110b7a7bfa78f334e402c5445d887d
Namira Pramadhani <namnam6617@gmail.com>
2025-01-25 22:04:54 +0700

    membuat data class DetailKursusViewModel

--- app/src/main/java/com/example/a12_081/ui/ViewModel/Kursus/DetailKursusViewModel.kt
+++ app/src/main/java/com/example/a12_081/ui/ViewModel/Kursus/DetailKursusViewModel.kt
@@ -1,2 +1,13 @@
 package com.example.a12_081.ui.ViewModel.Kursus
 
+data class DetailKursusViewModel(
+    val detailKursusUiEvent: InsertKursusUiEvent = InsertKursusUiEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailKursusUiEvent == InsertKursusUiEvent()
+    val isUiEventNotEmpty: Boolean
+        get() = detailKursusUiEvent != InsertKursusUiEvent()
+}
