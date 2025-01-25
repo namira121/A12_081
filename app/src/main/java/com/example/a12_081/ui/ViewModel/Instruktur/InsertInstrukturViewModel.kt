3b2c37d46268dda3dd6bc210ef960a7985740221
Namira Pramadhani <namnam6617@gmail.com>
2025-01-25 21:07:24 +0700

    membuat class ViewModel dan parameter ins untuk mengakses repository instruktur. membuat variabel untuk menyimpan state ui

--- app/src/main/java/com/example/a12_081/ui/ViewModel/Instruktur/InsertInstrukturViewModel.kt
+++ app/src/main/java/com/example/a12_081/ui/ViewModel/Instruktur/InsertInstrukturViewModel.kt
@@ -1,6 +1,11 @@
 package com.example.a12_081.ui.ViewModel.Instruktur
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.a12_081.model.instruktur
+import com.example.a12_081.repository.InstrukturRepository
 
 data class InsertInstrukturUiEvent(
     val id_instruktur: String= "",
@@ -33,3 +38,8 @@ fun instruktur.toInsertInstrukturUiEvent():InsertInstrukturUiEvent = InsertInstr
 fun instruktur.toUiStateIns(): InsertInstrukturUiState = InsertInstrukturUiState(
     insertInstrukturUiEvent = toInsertInstrukturUiEvent()
 )
+
+class InsertInstrukturViewModel (private val ins: InstrukturRepository): ViewModel(){
+    var InsUiState by mutableStateOf(InsertInstrukturUiState())
+        private set
+}
