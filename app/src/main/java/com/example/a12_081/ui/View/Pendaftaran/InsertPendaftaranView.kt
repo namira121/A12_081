1ac8cef15c4239d04906dca58c8f2bb279028a17
Namira Pramadhani <namnam6617@gmail.com>
2025-01-26 00:58:53 +0700

    Membuat DestinasiInsertPendaftaran di insert view

--- app/src/main/java/com/example/a12_081/ui/View/Pendaftaran/InsertPendaftaranView.kt
+++ app/src/main/java/com/example/a12_081/ui/View/Pendaftaran/InsertPendaftaranView.kt
@@ -1,2 +1,8 @@
 package com.example.a12_081.ui.View.Pendaftaran
 
+import com.example.a12_081.ui.Navigation.AlamatNavigasi
+
+object DestinasiInsertPendaftaran: AlamatNavigasi {
+    override val route= "InsertPendaftaran"
+    override val titleRes = "Tambah Pendaftaran"
+}
