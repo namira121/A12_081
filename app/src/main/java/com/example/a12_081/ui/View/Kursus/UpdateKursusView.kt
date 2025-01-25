f01fa2dbaa74189cc7dfbe4bec7587d6fe2352c6
Namira Pramadhani <namnam6617@gmail.com>
2025-01-26 00:55:57 +0700

    Membuat DestinasiUpdateKursus di update view

--- app/src/main/java/com/example/a12_081/ui/View/Kursus/UpdateKursusView.kt
+++ app/src/main/java/com/example/a12_081/ui/View/Kursus/UpdateKursusView.kt
@@ -1,2 +1,10 @@
 package com.example.a12_081.ui.View.Kursus
 
+import com.example.a12_081.ui.Navigation.AlamatNavigasi
+
+object DestinasiUpdateKursus: AlamatNavigasi {
+    override val route= "UpdateKursus"
+    override val titleRes= "Update Kursus"
+    const val ID_KURSUS = "id_kursus"
+    val routeWithArgs = "$route/{$ID_KURSUS}"
+}
