d98da8cd9097ff079e76accbc8e4762679914042
Namira Pramadhani <namnam6617@gmail.com>
2025-01-25 22:48:10 +0700

    membuat AlamatNavigasi dan menambahkan DestinasiHome di halaman home utama

---
+++ app/src/main/java/com/example/a12_081/ui/Navigation/AlamatNavigasi.kt
@@ -0,0 +1,7 @@
+package com.example.a12_081.ui.Navigation
+
+interface AlamatNavigasi{
+    val route: String
+    val titleRes: String
+}
+
--- app/src/main/java/com/example/a12_081/ui/ViewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/a12_081/ui/ViewModel/HomeViewModel.kt
@@ -1,2 +1,8 @@
 package com.example.a12_081.ui.ViewModel
 
+import com.example.a12_081.ui.Navigation.AlamatNavigasi
+
+object DestinasiHome: AlamatNavigasi {
+    override val route= "home"
+    override val titleRes= "Main Home"
+}
