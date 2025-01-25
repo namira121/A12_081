1f165068baf30cdc48506227bd6db47b5793077d
Namira Pramadhani <namnam6617@gmail.com>
2025-01-25 22:22:05 +0700

    membuat data class DetailInstrukturUiState

--- app/src/main/java/com/example/a12_081/ui/ViewModel/Instruktur/DetailInstrukturViewModel.kt
+++ app/src/main/java/com/example/a12_081/ui/ViewModel/Instruktur/DetailInstrukturViewModel.kt
@@ -1,2 +1,14 @@
 package com.example.a12_081.ui.ViewModel.Instruktur
 
+data class DetailInstrukturUiState(
+    val detailInstrukturUiEvent: InsertInstrukturUiEvent = InsertInstrukturUiEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailInstrukturUiEvent == InsertInstrukturUiEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailInstrukturUiEvent != InsertInstrukturUiEvent()
+}
