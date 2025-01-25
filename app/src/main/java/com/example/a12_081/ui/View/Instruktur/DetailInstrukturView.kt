34b39247f44675bbd8ae69b76e37634f3eb66674
Namira Pramadhani <namnam6617@gmail.com>
2025-01-26 01:11:27 +0700

    Menambahkan alamat navigasi untuk halaman DetailInstrukturView.kt

--- app/src/main/java/com/example/a12_081/ui/View/Instruktur/DetailInstrukturView.kt
+++ app/src/main/java/com/example/a12_081/ui/View/Instruktur/DetailInstrukturView.kt
@@ -1,2 +1,10 @@
 package com.example.a12_081.ui.View.Instruktur
 
+import com.example.a12_081.ui.Navigation.AlamatNavigasi
+
+object DestinasiDetailInstruktur: AlamatNavigasi {
+    override val route= "DetailInstruktur"
+    override val titleRes= "Detail Data Instruktur"
+    const val ID_INSTRUKTUR = "id_instruktur"
+    val routeWithArgs = "$route/{$ID_INSTRUKTUR}"
+}
