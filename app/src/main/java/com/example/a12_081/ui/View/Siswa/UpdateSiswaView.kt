b88a3d70621019d2ed26a5165932f4f6188fda96
Namira Pramadhani <namnam6617@gmail.com>
2025-01-25 22:51:14 +0700

    Menambahkan DestinasiUpdateSiswa di halaman UpdateSiswaView.kt

--- app/src/main/java/com/example/a12_081/ui/View/Siswa/UpdateSiswaView.kt
+++ app/src/main/java/com/example/a12_081/ui/View/Siswa/UpdateSiswaView.kt
@@ -1,2 +1,10 @@
 package com.example.a12_081.ui.View.Siswa
 
+import com.example.a12_081.ui.Navigation.AlamatNavigasi
+
+object DestinasiUpdateSiswa: AlamatNavigasi {
+    override val route= "UpdateSiswa"
+    override val titleRes= "Update Siswa"
+    const val ID_SISWA = "id_siswa"
+    val routeWithArgs = "$route/{$ID_SISWA}"
+}
