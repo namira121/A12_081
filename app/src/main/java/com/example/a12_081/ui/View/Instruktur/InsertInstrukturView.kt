4672fed52b765f122397cb72c05ab6c33d0278ca
Namira Pramadhani <namnam6617@gmail.com>
2025-01-26 01:04:53 +0700

    Membuat DestinasiInsertInstruktur di insert view

--- app/src/main/java/com/example/a12_081/ui/View/Instruktur/InsertInstrukturView.kt
+++ app/src/main/java/com/example/a12_081/ui/View/Instruktur/InsertInstrukturView.kt
@@ -1,2 +1,8 @@
 package com.example.a12_081.ui.View.Instruktur
 
+import com.example.a12_081.ui.Navigation.AlamatNavigasi
+
+object DestinasiInsertInstruktur: AlamatNavigasi {
+    override val route= "InsertInstruktur"
+    override val titleRes = "Tambah Instruktur"
+}
