b5823916b305c3264952ece7104dc98506e59e12
Namira Pramadhani <namnam6617@gmail.com>
2025-01-25 21:35:32 +0700

    mengambil data kursus dengan fungsi getkursusbyid

--- app/src/main/java/com/example/a12_081/ui/ViewModel/Kursus/UpdateKursusViewModel.kt
+++ app/src/main/java/com/example/a12_081/ui/ViewModel/Kursus/UpdateKursusViewModel.kt
@@ -5,8 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.a12_081.model.kursus
 import com.example.a12_081.repository.KursusRepository
+import kotlinx.coroutines.launch
 
 class UpdateKursusViewModel (savedStateHandle: SavedStateHandle, private val kursusRepository: KursusRepository): ViewModel(){
 
@@ -15,5 +17,10 @@ class UpdateKursusViewModel (savedStateHandle: SavedStateHandle, private val kur
 
     private val _id_kursus: String = checkNotNull(savedStateHandle[DestinasiUpdateKursus.ID_KURSUS])
 
-
+    init {
+        viewModelScope.launch {
+            updateKursusUiState = kursusRepository.getKursusByID(_id_kursus)
+                .toUiStateKrs()
+        }
+    }
 }
