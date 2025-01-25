8fe7b77a06d0f11548c6df3a9db163c745769457
Namira Pramadhani <namnam6617@gmail.com>
2025-01-25 21:50:07 +0700

    Membuat class viewmodel untuk instruktur dan membuat variabel untuk menyimpan statenya

--- app/src/main/java/com/example/a12_081/ui/ViewModel/Instruktur/UpdateInstrukturViewModel.kt
+++ app/src/main/java/com/example/a12_081/ui/ViewModel/Instruktur/UpdateInstrukturViewModel.kt
@@ -1,2 +1,17 @@
 package com.example.a12_081.ui.ViewModel.Instruktur
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.a12_081.repository.InstrukturRepository
+
+class UpdateInstrukturViewModel(savedStateHandle: SavedStateHandle,private val instrukturRepository: InstrukturRepository): ViewModel(){
+    var updateInstrukturUiState by mutableStateOf(InsertInstrukturUiState())
+        private set
+
+    private val _id_instruktur: String = checkNotNull(savedStateHandle[DestinasiUpdateInstruktur.ID_INSTRUKTUR])
+
+
+}
