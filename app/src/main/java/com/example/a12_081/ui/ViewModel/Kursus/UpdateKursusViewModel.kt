8ccc2e581736a2210b876f16aeeda5143814d184
Namira Pramadhani <namnam6617@gmail.com>
2025-01-25 21:30:50 +0700

    membuat class UpdateKursusViewModel dengan parameter kursusRepository dan savedhandlestate

--- app/src/main/java/com/example/a12_081/ui/ViewModel/Kursus/UpdateKursusViewModel.kt
+++ app/src/main/java/com/example/a12_081/ui/ViewModel/Kursus/UpdateKursusViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.a12_081.ui.ViewModel.Kursus
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.a12_081.model.kursus
+import com.example.a12_081.repository.KursusRepository
+
+class UpdateKursusViewModel (savedStateHandle: SavedStateHandle, private val kursusRepository: KursusRepository): ViewModel(){
+
+}
