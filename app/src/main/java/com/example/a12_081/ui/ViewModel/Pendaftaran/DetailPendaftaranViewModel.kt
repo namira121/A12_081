3b0192cd4922aa60ea490c7738867a25b24b2dff
Namira Pramadhani <namnam6617@gmail.com>
2025-01-25 18:09:38 +0700

    membuat fungsi untuk transformasi dari pendaftaran ke dalam insertDaftarUiEvent, lalu mengembalikan nilai-nilai yang diambil

--- app/src/main/java/com/example/a12_081/ui/ViewModel/Pendaftaran/DetailPendaftaranViewModel.kt
+++ app/src/main/java/com/example/a12_081/ui/ViewModel/Pendaftaran/DetailPendaftaranViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.a12_081.ui.ViewModel.Pendaftaran
 
+import com.example.a12_081.model.pendaftaran
+
 data class DetailDaftarUiState(
     val detailDaftarUiState: InsertDaftarUiEvent = InsertDaftarUiEvent(),
     val isLoading: Boolean = false,
@@ -11,4 +13,13 @@ data class DetailDaftarUiState(
 
     val isUiEventNotEmpty: Boolean
         get() = detailDaftarUiState != InsertDaftarUiEvent()
+}
+
+fun pendaftaran.toDetailDaftarUiEvent(): InsertDaftarUiEvent{
+    return InsertDaftarUiEvent(
+        id_pendaftaran =id_pendaftaran,
+        id_siswa=id_siswa,
+        id_kursus=id_kursus,
+        tanggal_pendaftaran=tanggal_pendaftaran
+    )
 }
