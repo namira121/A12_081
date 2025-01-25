51afc9e05d52bd2a062e0f6c19beff8c22e9a39a
Namira Pramadhani <namnam6617@gmail.com>
2025-01-26 00:54:41 +0700

    Membuat DestinasiUpdatePendaftaran di update view

--- app/src/main/java/com/example/a12_081/ui/View/Pendaftaran/UpdatePendaftaranView.kt
+++ app/src/main/java/com/example/a12_081/ui/View/Pendaftaran/UpdatePendaftaranView.kt
@@ -1,2 +1,10 @@
 package com.example.a12_081.ui.View.Pendaftaran
 
+import com.example.a12_081.ui.Navigation.AlamatNavigasi
+
+object DestinasiUpdatePendaftaran: AlamatNavigasi {
+    override val route= "UpdatePendaftaran"
+    override val titleRes= "Update Pendaftaran"
+    const val ID_PENDAFTARAN = "id_pendaftaran"
+    val routeWithArgs = "$route/{$ID_PENDAFTARAN}"
+}
