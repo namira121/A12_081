8f6c60fc2058a4a3145a639143dd79a28a8ac43d
Namira Pramadhani <namnam6617@gmail.com>
2025-01-25 18:01:28 +0700

    membuat variabel ketika DetailDaftarUiState terisi dan tidak terisi

--- app/src/main/java/com/example/a12_081/ui/ViewModel/Pendaftaran/DetailPendaftaranViewModel.kt
+++ app/src/main/java/com/example/a12_081/ui/ViewModel/Pendaftaran/DetailPendaftaranViewModel.kt
@@ -2,4 +2,13 @@ package com.example.a12_081.ui.ViewModel.Pendaftaran
 
 data class DetailDaftarUiState(
     val detailDaftarUiState: InsertDaftarUiEvent = InsertDaftarUiEvent(),
-){}
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errormessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailDaftarUiState == InsertDaftarUiEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailDaftarUiState != InsertDaftarUiEvent()
+}
