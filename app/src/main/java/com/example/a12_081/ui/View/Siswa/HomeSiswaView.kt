6a5afc4859ce5c503fc18bc93d1f6cb7855493f3
Namira Pramadhani <namnam6617@gmail.com>
2025-01-25 22:53:57 +0700

    Menambahkan DestinasiHomeSiswa di halaman HomeSiswaView.kt

--- app/src/main/java/com/example/a12_081/ui/View/Siswa/HomeSiswaView.kt
+++ app/src/main/java/com/example/a12_081/ui/View/Siswa/HomeSiswaView.kt
@@ -1,2 +1,8 @@
 package com.example.a12_081.ui.View.Siswa
 
+import com.example.a12_081.ui.Navigation.AlamatNavigasi
+
+object DestinasiHomeSiswa: AlamatNavigasi{
+    override val route= "HomeSiswa"
+    override val titleRes = "Home Siswa"
+}
