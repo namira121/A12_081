5155dc11360d022b75504a12e86d8a5dd896c3c0
Namira Pramadhani <namnam6617@gmail.com>
2025-01-25 20:51:13 +0700

    membuat data class InsertInstrukturEvent untuk data yang akan ditambahkan

--- app/src/main/java/com/example/a12_081/ui/ViewModel/Instruktur/InsertInstrukturViewModel.kt
+++ app/src/main/java/com/example/a12_081/ui/ViewModel/Instruktur/InsertInstrukturViewModel.kt
@@ -1,2 +1,9 @@
 package com.example.a12_081.ui.ViewModel.Instruktur
 
+data class InsertInstrukturEvent(
+    val id_instruktur: String= "",
+    val nama_instruktur: String= "",
+    val email: String= "",
+    val nomor_telepon: String= "",
+    val deskripsi: String= "",
+)
