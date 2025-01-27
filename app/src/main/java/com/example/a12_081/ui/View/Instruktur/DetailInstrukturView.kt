3e63ed2ba9b19843df695ac4de2a65bca9a0ff91
Namira Pramadhani <namnam6617@gmail.com>
2025-01-27 22:07:06 +0700

    membuat DetailInstrukturView untuk menampilkan detail data intruktur

--- app/src/main/java/com/example/a12_081/ui/View/Instruktur/DetailInstrukturView.kt
+++ app/src/main/java/com/example/a12_081/ui/View/Instruktur/DetailInstrukturView.kt
@@ -1,10 +1,239 @@
 package com.example.a12_081.ui.View.Instruktur
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.a12_081.model.instruktur
+import com.example.a12_081.model.siswa
+import com.example.a12_081.ui.CustomWidget.CustomTopAppBar
 import com.example.a12_081.ui.Navigation.AlamatNavigasi
+import com.example.a12_081.ui.View.Siswa.DestinasiDetailSiswa
+import com.example.a12_081.ui.ViewModel.Instruktur.DetailInstrukturUiState
+import com.example.a12_081.ui.ViewModel.Instruktur.DetailInstrukturViewModel
+import com.example.a12_081.ui.ViewModel.Instruktur.toIns
+import com.example.a12_081.ui.ViewModel.PenyediaViewModel
+import com.example.a12_081.ui.ViewModel.Siswa.DetailSiswaUiState
+import com.example.a12_081.ui.ViewModel.Siswa.DetailSiswaViewModel
+import com.example.a12_081.ui.ViewModel.Siswa.toSw
 
 object DestinasiDetailInstruktur: AlamatNavigasi {
     override val route= "DetailInstruktur"
     override val titleRes= "Detail Data Instruktur"
     const val ID_INSTRUKTUR = "id_instruktur"
     val routeWithArgs = "$route/{$ID_INSTRUKTUR}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailInstrukturView(
+    modifier: Modifier = Modifier,
+    NavigateBack: () -> Unit,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit = { },
+    viewModel: DetailInstrukturViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomTopAppBar(
+                title = DestinasiDetailInstruktur.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = NavigateBack
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onEditClick,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Instruktur"
+                )
+            }
+        }
+    ) { innerPadding ->
+        var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+
+        BodyDetailInstruktur(
+            detailInstrukturUiState = viewModel.detailInstrukturUiState,
+            modifier = Modifier.padding(innerPadding),
+            onDeleteClick = {
+                deleteConfirmationRequired = true
+            }
+        )
+
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    viewModel.deleteIns()
+                    onDeleteClick()
+                    deleteConfirmationRequired = false
+                },
+                onDeleteCancel = {
+                    deleteConfirmationRequired = false
+                },
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun BodyDetailInstruktur(
+    modifier: Modifier = Modifier,
+    detailInstrukturUiState: DetailInstrukturUiState,
+    onDeleteClick: () -> Unit
+) {
+    when {
+        detailInstrukturUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+        detailInstrukturUiState.isError -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = detailInstrukturUiState.errorMessage,
+                    color = Color.Red
+                )
+            }
+        }
+        detailInstrukturUiState.isUiEventNotEmpty -> {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailInstruktur(
+                    instruktur = detailInstrukturUiState.detailInstrukturUiEvent.toIns(),
+                    modifier = modifier
+                )
+
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button(
+                    onClick = onDeleteClick,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ItemDetailInstruktur(
+    modifier: Modifier = Modifier,
+    instruktur: instruktur,
+){
+    Card(
+        modifier = modifier.fillMaxWidth().padding(top = 20.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ){
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            ComponentDetailIns(judul = "ID", isinya = instruktur.id_instruktur)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailIns(judul = "Nama", isinya = instruktur.nama_instruktur)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailIns(judul = "Email", isinya = instruktur.email)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailIns(judul = "Nomor Telepon", isinya = instruktur.nomor_telepon)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailIns(judul = "Deskripsi", isinya = instruktur.deskripsi)
+            Spacer(modifier = Modifier.padding(4.dp))
+        }
+    }
+}
+
+@Composable
+fun ComponentDetailIns(
+    modifier: Modifier = Modifier,
+    judul:String,
+    isinya:String
+){
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    AlertDialog(onDismissRequest = {  },
+        title = { Text("Delete Data") },
+        text = { Text("Apakah anda yakin ingin menghapus data?") },
+        dismissButton = {
+            TextButton(onClick = { onDeleteCancel() }) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = { onDeleteConfirm() }) {
+                Text(text = "Yes")
+            }
+        }
+    )
 }
