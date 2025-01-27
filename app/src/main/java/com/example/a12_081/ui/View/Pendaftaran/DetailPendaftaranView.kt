f7a121eb184346a00b601d982ff28534dc996361
Namira Pramadhani <namnam6617@gmail.com>
2025-01-27 19:00:43 +0700

    membuat DetailPendaftaranView untuk menampilkan detail data pendaftaran

--- app/src/main/java/com/example/a12_081/ui/View/Pendaftaran/DetailPendaftaranView.kt
+++ app/src/main/java/com/example/a12_081/ui/View/Pendaftaran/DetailPendaftaranView.kt
@@ -1,10 +1,237 @@
 package com.example.a12_081.ui.View.Pendaftaran
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.a12_081.model.pendaftaran
+import com.example.a12_081.model.siswa
+import com.example.a12_081.ui.CustomWidget.CustomTopAppBar
 import com.example.a12_081.ui.Navigation.AlamatNavigasi
+import com.example.a12_081.ui.View.Siswa.DestinasiDetailSiswa
+import com.example.a12_081.ui.ViewModel.Pendaftaran.DetailDaftarUiState
+import com.example.a12_081.ui.ViewModel.Pendaftaran.DetailPendaftaranViewModel
+import com.example.a12_081.ui.ViewModel.Pendaftaran.toDtr
+import com.example.a12_081.ui.ViewModel.PenyediaViewModel
+import com.example.a12_081.ui.ViewModel.Siswa.DetailSiswaUiState
+import com.example.a12_081.ui.ViewModel.Siswa.DetailSiswaViewModel
+import com.example.a12_081.ui.ViewModel.Siswa.toSw
 
 object DestinasiDetailPendaftaran: AlamatNavigasi {
     override val route= "DetailPendaftaran"
     override val titleRes= "Detail Data Pendaftaran"
     const val ID_PENDAFTARAN = "id_pendaftaran"
     val routeWithArgs = "$route/{$ID_PENDAFTARAN}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailPendaftaranView(
+    modifier: Modifier = Modifier,
+    NavigateBack: () -> Unit,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit = { },
+    viewModel: DetailPendaftaranViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomTopAppBar(
+                title = DestinasiDetailPendaftaran.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = NavigateBack
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onEditClick,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Pendaftaran"
+                )
+            }
+        }
+    ) { innerPadding ->
+        var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+
+        BodyDetailDaftar(
+            detailDaftarUiState = viewModel.detailDaftarUiState,
+            modifier = Modifier.padding(innerPadding),
+            onDeleteClick = {
+                deleteConfirmationRequired = true
+            }
+        )
+
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    viewModel.deleteDtr()
+                    onDeleteClick()
+                    deleteConfirmationRequired = false
+                },
+                onDeleteCancel = {
+                    deleteConfirmationRequired = false
+                },
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun BodyDetailDaftar(
+    modifier: Modifier = Modifier,
+    detailDaftarUiState: DetailDaftarUiState,
+    onDeleteClick: () -> Unit
+) {
+    when {
+        detailDaftarUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+        detailDaftarUiState.isError -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = detailDaftarUiState.errormessage,
+                    color = Color.Red
+                )
+            }
+        }
+        detailDaftarUiState.isUiEventNotEmpty -> {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailDaftar(
+                    pendaftaran = detailDaftarUiState.detailDaftarUiEvent.toDtr(),
+                    modifier = modifier
+                )
+
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button(
+                    onClick = onDeleteClick,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ItemDetailDaftar(
+    modifier: Modifier = Modifier,
+    pendaftaran: pendaftaran,
+){
+    Card(
+        modifier = modifier.fillMaxWidth().padding(top = 20.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ){
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            ComponentDetailDtr(judul = "ID pendaftaran", isinya = pendaftaran.id_pendaftaran)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailDtr(judul = "ID siswa", isinya = pendaftaran.id_siswa)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailDtr(judul = "ID Kursus", isinya = pendaftaran.id_kursus)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailDtr(judul = "Tanggal Pendaftaran", isinya = pendaftaran.tanggal_pendaftaran)
+            Spacer(modifier = Modifier.padding(4.dp))
+        }
+    }
+}
+
+@Composable
+fun ComponentDetailDtr(
+    modifier: Modifier = Modifier,
+    judul:String,
+    isinya:String
+){
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    AlertDialog(onDismissRequest = {  },
+        title = { Text("Delete Data") },
+        text = { Text("Apakah anda yakin ingin menghapus data?") },
+        dismissButton = {
+            TextButton(onClick = { onDeleteCancel() }) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = { onDeleteConfirm() }) {
+                Text(text = "Yes")
+            }
+        }
+    )
 }
