a034e1c8c254e4bcb853f682c1293c9a2360b41f
Namira Pramadhani <namnam6617@gmail.com>
2025-01-27 20:19:40 +0700

    membuat DetailInstrukturView untuk menampilkan detail data kursus

--- app/src/main/java/com/example/a12_081/ui/View/Kursus/DetailKursusView.kt
+++ app/src/main/java/com/example/a12_081/ui/View/Kursus/DetailKursusView.kt
@@ -1,10 +1,241 @@
 package com.example.a12_081.ui.View.Kursus
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.a12_081.model.kursus
+import com.example.a12_081.model.siswa
+import com.example.a12_081.ui.CustomWidget.CustomTopAppBar
 import com.example.a12_081.ui.Navigation.AlamatNavigasi
+import com.example.a12_081.ui.View.Siswa.DestinasiDetailSiswa
+import com.example.a12_081.ui.ViewModel.Kursus.DetailKursusUiState
+import com.example.a12_081.ui.ViewModel.Kursus.DetailKursusViewModel
+import com.example.a12_081.ui.ViewModel.Kursus.toKrs
+import com.example.a12_081.ui.ViewModel.PenyediaViewModel
+import com.example.a12_081.ui.ViewModel.Siswa.DetailSiswaUiState
+import com.example.a12_081.ui.ViewModel.Siswa.DetailSiswaViewModel
+import com.example.a12_081.ui.ViewModel.Siswa.toSw
 
 object DestinasiDetailKursus: AlamatNavigasi {
     override val route= "DetailKursus"
     override val titleRes= "Detail Data Kursus"
     const val ID_KURSUS = "id_kursus"
     val routeWithArgs = "$route/{$ID_KURSUS}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailKursusView(
+    modifier: Modifier = Modifier,
+    NavigateBack: () -> Unit,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit = { },
+    viewModel: DetailKursusViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomTopAppBar(
+                title = DestinasiDetailKursus.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = NavigateBack
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onEditClick,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Kursus"
+                )
+            }
+        }
+    ) { innerPadding ->
+        var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+
+        BodyDetailKursus(
+            detailKursusUiState = viewModel.detailKursusUiState,
+            modifier = Modifier.padding(innerPadding),
+            onDeleteClick = {
+                deleteConfirmationRequired = true
+            }
+        )
+
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    viewModel.deleteKrs()
+                    onDeleteClick()
+                    deleteConfirmationRequired = false
+                },
+                onDeleteCancel = {
+                    deleteConfirmationRequired = false
+                },
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun BodyDetailKursus(
+    modifier: Modifier = Modifier,
+    detailKursusUiState: DetailKursusUiState,
+    onDeleteClick: () -> Unit
+) {
+    when {
+        detailKursusUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+        detailKursusUiState.isError -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = detailKursusUiState.errorMessage,
+                    color = Color.Red
+                )
+            }
+        }
+        detailKursusUiState.isUiEventNotEmpty -> {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailKursus(
+                    kursus = detailKursusUiState.detailKursusUiEvent.toKrs(),
+                    modifier = modifier
+                )
+
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button(
+                    onClick = onDeleteClick,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ItemDetailKursus(
+    modifier: Modifier = Modifier,
+    kursus: kursus,
+){
+    Card(
+        modifier = modifier.fillMaxWidth().padding(top = 20.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ){
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            ComponentDetailKrs(judul = "ID", isinya = kursus.id_kursus)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailKrs(judul = "Nama", isinya = kursus.nama_kursus)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailKrs(judul = "Kategori", isinya = kursus.kategori)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailKrs(judul = "Harga", isinya = kursus.harga)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailKrs(judul = "Deskripsi", isinya = kursus.deskripsi)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailKrs(judul = "Instruktur", isinya = kursus.id_instruktur)
+            Spacer(modifier = Modifier.padding(4.dp))
+        }
+    }
+}
+
+@Composable
+fun ComponentDetailKrs(
+    modifier: Modifier = Modifier,
+    judul:String,
+    isinya:String
+){
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    AlertDialog(onDismissRequest = {  },
+        title = { Text("Delete Data") },
+        text = { Text("Apakah anda yakin ingin menghapus data?") },
+        dismissButton = {
+            TextButton(onClick = { onDeleteCancel() }) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = { onDeleteConfirm() }) {
+                Text(text = "Yes")
+            }
+        }
+    )
 }
