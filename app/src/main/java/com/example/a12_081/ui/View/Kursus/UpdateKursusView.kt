1db07f8c91da175a5215fd93333884afba8072c4
Namira Pramadhani <namnam6617@gmail.com>
2025-01-27 10:10:58 +0700

    membuat UpdateKursusView agar user dapat memperbarui data kursus yang sudah dimasukkan

--- app/src/main/java/com/example/a12_081/ui/View/Kursus/UpdateKursusView.kt
+++ app/src/main/java/com/example/a12_081/ui/View/Kursus/UpdateKursusView.kt
@@ -1,10 +1,58 @@
 package com.example.a12_081.ui.View.Kursus
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.a12_081.ui.CustomWidget.CustomTopAppBar
 import com.example.a12_081.ui.Navigation.AlamatNavigasi
+import com.example.a12_081.ui.View.Pendaftaran.DestinasiUpdatePendaftaran
+import com.example.a12_081.ui.View.Pendaftaran.InsertDaftarBody
+import com.example.a12_081.ui.ViewModel.Kursus.UpdateKursusViewModel
+import com.example.a12_081.ui.ViewModel.Pendaftaran.UpdateDaftarViewModel
+import com.example.a12_081.ui.ViewModel.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 object DestinasiUpdateKursus: AlamatNavigasi {
     override val route= "UpdateKursus"
     override val titleRes= "Update Kursus"
     const val ID_KURSUS = "id_kursus"
     val routeWithArgs = "$route/{$ID_KURSUS}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateKursusView(
+    id_kursus: String,
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    updateViewModel: UpdateKursusViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            CustomTopAppBar(
+                title = DestinasiUpdateKursus.titleRes,
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        InsertKursusBody(
+            insertKursusUiState = updateViewModel.updateKursusUiState,
+            onKursusValueChange = updateViewModel::updateInsertKrsState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    updateViewModel.updateKrs()
+                    onNavigateUp()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
 }
